fd16dde2bc5e9c4b386f6007cbec340a3f91ea5a
Justin Barnard <justinbarnard@users.noreply.github.com>
2021-03-07 10:39:28 -0600

    WIP: baseline

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import './App.css';
+
+interface AppProps {}
+
+function App({}: AppProps) {
+  // Return the App component.
+  return null
+}
+
+export default App;
---
+++ src/Components/AppBar.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import AppBar from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles'
+
+const useStyles = makeStyles((theme)=> {
+
+});
+
+export default function PLM_AppBar() {
+    const classes = useStyles();
+
+    return (
+        <div>
+            <AppBar>
+                
+            </AppBar>
+        </div>
+    )
+
+}
